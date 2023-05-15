6a2ed4ce97a861b46204a7c78fbd9843e696dacf
Divyanshu <Divyanshu123123@gmail.com>
2023-05-15 05:40:02 +0530

    final commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import App1 from './coinPriceList.tsx';
+
+function App() {
+  return (
+    <div className="App">
+      <App1/>
+    </div>
+  );
+}
+
+export default App;
